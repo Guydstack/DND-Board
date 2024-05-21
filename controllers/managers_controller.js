ef9a02967d00228f6a4d1a1a14bb78c179da9cc7
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-05-22 01:05:10 +0300

    Update managers_controller.js

--- controllers/managers_controller.js
+++ controllers/managers_controller.js
@@ -157,7 +157,7 @@ module.exports = {
       await Model.findByIdAndUpdate(manager._id, {
         tokens: [...oldTokens, { token, signedAt: Date.now().toString() }],
       }).exec();
-       res.cookie('token',token,{ maxAge :1000 * 60 * 60 , sameSite: 'None', secure: true})
+       res.cookie('token',token,{ maxAge :1000 * 60 * 60 , httpOnly: true})
       return res.status(201).json({
         success: true,
         message: "login seccessfully",
