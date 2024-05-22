3328153ffc13c39851f9365bdfdee37d67af34e6
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-05-22 23:38:33 +0300

    Update managers_controller.js

--- controllers/managers_controller.js
+++ controllers/managers_controller.js
@@ -200,8 +200,7 @@ module.exports = {
 
   authManager: async (req, res) => {
     try {
-      const token = req.headers.authorization?.split(' ')[1];
-      // const token = req.cookies?.token
+      const token = req.cookies?.token
 
       if (!token) {
         throw new Error("no token provided");
