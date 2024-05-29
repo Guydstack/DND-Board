d07c0e01dc62269c182968e43d20d6a52ea2ae27
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-05-29 20:31:50 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,14 +5,11 @@ const mongoose = require('mongoose');
 const connection = async() =>{
 
     let url = process.env.MONGO_URI;
-    //|| "mongodb://127.0.0.1:27017/master"
-
     try {
         
         await mongoose.connect(url,{
             useUnifiedTopology: true,
             useNewUrlParser: true,
-            /* useCreateIndex: true, */
             autoIndex: true,
         });
 
