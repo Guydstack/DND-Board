4388c02adee3d7cffeb286b63f9649ecad39266a
Guydstack <129586924+Guydstack@users.noreply.github.com>
2025-05-26 17:06:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const contact_router = require("./routes/contact_router");
 const allowedOrigins = [
   "https://dnd-board-admin.onrender.com",
   "https://dnd-board-client.onrender.com",
-  "https://dnd-frontend-test.onrender.com"
+  "https://dnd-frontend-test.onrender.com",
+  "https://dnd-board.com"
 ];
 
 
